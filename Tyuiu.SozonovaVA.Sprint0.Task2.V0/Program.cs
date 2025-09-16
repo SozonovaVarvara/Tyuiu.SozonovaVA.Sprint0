36fb752c2071bb602128fa55b8b19b49fbe45536
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-09-17 01:26:32 +0500

    Task2

---
+++ Tyuiu.SozonovaVA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿using Tyuiu.SozovovaVA.Sprint0.Task2.V0.Lib;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine(DataService.GetMessage("Варвара"));
+Console.ReadKey ();
