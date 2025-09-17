175cf727597209cd9ef126d44144892eb487cd84
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-09-17 06:36:13 +0500

    Task6

---
+++ Tyuiu.SozonovaVA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.SozonovaVA.Sprint0.Task6.V0.Lib;
+
+// See https://aka.ms/new-console-template for more information
+int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray);
+Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArray(numsArray);
+Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray);
+Console.ReadLine();
