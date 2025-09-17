60ab19eb1c914319ed485406e5a7195b492db295
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-09-17 05:29:18 +0500

    Task5

---
+++ Tyuiu.SozonovaVA.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+using Tyuiu.SozonovaVA.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.SozonovaVA.Sprint0.Task5.V0.Test
+{
+    public class DataServiceTest
+    {
+        [Fact]
+        public void CheckAdditionValid()
+        {
+            Assert.Equal(10, DataService.Addition(5, 5));
+        }
+        public void CheckSubtractionValid()
+        {
+            Assert.Equal(5, DataService.Subtraction(10, 5));
+        }
+
+        public void CheckMultiplicationValid()
+        {
+            Assert.Equal(50, DataService.Multiplication(10, 5));
+        }
+
+        public void CheckDivisionValid()
+        {
+            Assert.Equal(3, DataService.Division(9, 3));
+        }
+    }
+}
---
+++ Tyuiu.SozonovaVA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.SozonovaVA.Sprint0.Task5.V0.Lib;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("A + B = " + DataService.Addition(1, 5);
+Console.WriteLine("A - B = " + DataService.Substraction(15, 5);
+Console.WriteLine("A * B = " + DataService.Multiplication(10, 10);
+Console.WriteLine("A / B = " + DataService.Division(5, 0);
+Console.ReadLine;
+
